807b0e715d1d18b8dc804644f74c1c445b9093a7
curogihu <curoas@yahoo.co.jp>
2017-06-18 17:48:56 +0900

    Try something

---
+++ database/migrations/2017_06_18_080326_create_item_builds.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemBuilds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_builds', function (Blueprint $table) {
+            $table->bigIncrements("match_id");
+            $table->integer('main_version');
+            $table->integer('sub_version');
+            $table->integer('champion_id');
+            $table->char('role', 4);
+            $table->integer('item_id');
+            $table->integer('bought_time');
+            $table->timestamp('inserted_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('item_builds');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/import/summoner', function () {
+    return view('import.importSummoner');
+});
